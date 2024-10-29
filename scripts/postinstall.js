a5afe40a5e357b257f2729cc6cdcf4366cea017d
yushulx <lingxiao1002@gmail.com>
2024-10-29 16:19:07 +0800

    Fix dylib loading issue on macos

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -9,7 +9,8 @@ const commands_darwin = [
     "install_name_tool -change @rpath/libDynamsoftCore.dylib @loader_path/libDynamsoftCore.dylib build/Release/dbr.node",
     "install_name_tool -change @rpath/libDynamsoftLicense.dylib @loader_path/libDynamsoftLicense.dylib build/Release/dbr.node",
     "install_name_tool -change @rpath/libDynamsoftCaptureVisionRouter.dylib @loader_path/libDynamsoftCaptureVisionRouter.dylib build/Release/dbr.node",
-    "install_name_tool -change @rpath/libDynamsoftUtility.dylib @loader_path/libDynamsoftUtility.dylib build/Release/dbr.node"
+    "install_name_tool -change @rpath/libDynamsoftUtility.dylib @loader_path/libDynamsoftUtility.dylib build/Release/dbr.node",
+    "install_name_tool -change @rpath/libDynamsoftImageProcessing.dylib @loader_path/libDynamsoftImageProcessing.dylib build/Release/libDynamsoftUtility.dylib"
 ]
 
 const commands_windows = []
